15435ebf73e29d2b109857ea3d8503b8ec86990d
Shigekane.Akio <you@example.com>
2022-05-22 00:58:17 +0900

    rosnode hellow world

---
+++ .devcontainer/README.md
@@ -0,0 +1,12 @@
+# ROS test utilities 
+
+## Getting Started
+
+- Open by vscode
+- Launch devcontainer and run below
+
+```
+catkin_make
+roscore &
+rosrun test_utils virtual_lrf
+```
--- src/test_utils/src/virtual_lrf.cpp
+++ src/test_utils/src/virtual_lrf.cpp
@@ -1,5 +1,27 @@
-# include <iostream>
+#include <iostream>
+#include <ros/ros.h>
 
-int main(void){
-    std::cout << "Hellow world!" << "\n"; 
+int main(int argc, char** argv)
+{
+  std::cout << "Hellow world!"
+            << "\n";
+
+  ros::init(argc, argv, "virtual_lrf");
+  ros::NodeHandle n;
+  ros::Rate loop_rate(10);
+
+  int count = 0;
+  while (ros::ok())
+  {
+    ROS_DEBUG("log:%i", count);
+    ROS_INFO("log:%i", count);
+    ROS_WARN("log:%i", count);
+    ROS_ERROR("log:%i", count);
+    ROS_FATAL("log:%i", count);
+
+    ros::spinOnce();
+    loop_rate.sleep();
+    count++;
+  }
+  return 0;
 }
